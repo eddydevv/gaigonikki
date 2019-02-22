643780c94c12c4d1f9aa4d510bfb8e558c549ed0
Edwin Agbenyega <eddy.devv@gmail.com>
2019-02-21 22:07:06 -0700

    have to figure out the outer margin space

--- src/components/home/landing.js
+++ src/components/home/landing.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import Header from '../layouts/header'
+
+const page = { title: "Welcome to GaigoNikki" }
+
+const landingStyle = {
+  // backgroundImage: 'url("https://images.alphacoders.com/716/716962.jpg")',
+  backgroundColor: "rgba(122,232,12,0.3)",
+  // backgroundSize: "cover"
+}
 
 export default class Landing extends Component {
   render() {
     return (
-      <div>
-        Landing Page!
+      <div style={landingStyle}>
+        <Header title={page.title}/>
       </div>
     )
   }
---
+++ src/components/layouts/header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const headerStyle = {
+  fontSize: "2.4em",
+  fontFamily: ["Athiti", "sans-serif"],
+  textAlign: "center",
+  padding: "20%",
+  color: "white",
+  backgroundColor: "rgba(30,30,30,0.5)"
+}
+
+const Header = props => {
+  return (
+    <header>
+      <h2 style={headerStyle}>{props.title}</h2>
+    </header>
+  )
+}
+
+export default Header
